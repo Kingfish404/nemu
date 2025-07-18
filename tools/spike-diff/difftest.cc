be36d0a339dfd1c5c3c487a0c03f199a7cd4e2a7
Yu Jin <jinyu929@qq.com>
2025-07-18 21:02:48 +0800

    feat: using upstream spike

--- tools/spike-diff/difftest.cc
+++ tools/spike-diff/difftest.cc
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include "mmu.h"
 #include "sim.h"
@@ -20,112 +20,161 @@
 
 #define NR_GPR MUXDEF(CONFIG_RVE, 16, 32)
 
-static std::vector<std::pair<reg_t, abstract_device_t*>> difftest_plugin_devices;
+static std::vector<device_factory_sargs_t> difftest_plugin_devices;
 static std::vector<std::string> difftest_htif_args;
-static std::vector<std::pair<reg_t, mem_t*>> difftest_mem(
+static std::vector<std::pair<reg_t, abstract_mem_t *>> difftest_mem(
     1, std::make_pair(reg_t(DRAM_BASE), new mem_t(CONFIG_MSIZE)));
 static debug_module_config_t difftest_dm_config = {
-  .progbufsize = 2,
-  .max_sba_data_width = 0,
-  .require_authentication = false,
-  .abstract_rti = 0,
-  .support_hasel = true,
-  .support_abstract_csr_access = true,
-  .support_abstract_fpr_access = true,
-  .support_haltgroups = true,
-  .support_impebreak = true
+    .progbufsize = 2,
+    .max_sba_data_width = 0,
+    .require_authentication = false,
+    .abstract_rti = 0,
+    .support_hasel = true,
+    .support_abstract_csr_access = true,
+    .support_abstract_fpr_access = true,
+    .support_haltgroups = true,
+    .support_impebreak = true,
 };
 
-struct diff_context_t {
+struct diff_context_t
+{
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   word_t pc;
 };
 
-static sim_t* s = NULL;
-static processor_t *p = NULL;
-static state_t *state = NULL;
+class diffsim_t : public sim_t
+{
+public:
+  processor_t *p;
+  state_t *state;
 
-void sim_t::diff_init(int port) {
-  p = get_core("0");
-  state = p->get_state();
-}
-
-void sim_t::diff_step(uint64_t n) {
-  step(n);
-}
-
-void sim_t::diff_get_regs(void* diff_context) {
-  struct diff_context_t* ctx = (struct diff_context_t*)diff_context;
-  for (int i = 0; i < NR_GPR; i++) {
-    ctx->gpr[i] = state->XPR[i];
+  diffsim_t(const cfg_t *cfg, bool halted,
+            std::vector<std::pair<reg_t, abstract_mem_t *>> mems,
+            const std::vector<device_factory_sargs_t> &plugin_device_factories,
+            const std::vector<std::string> &args,
+            const debug_module_config_t &dm_config,
+            const char *log_path,
+            bool dtb_enabled, const char *dtb_file,
+            bool socket_enabled,
+            FILE *cmd_file, // needed for command line option --cmd
+            std::optional<unsigned long long> instruction_limit)
+      : sim_t(cfg, halted, mems, plugin_device_factories, args,
+              dm_config, log_path, dtb_enabled, dtb_file,
+              socket_enabled, cmd_file, instruction_limit)
+  {
+    p = NULL;
+    state = NULL;
   }
-  ctx->pc = state->pc;
-}
 
-void sim_t::diff_set_regs(void* diff_context) {
-  struct diff_context_t* ctx = (struct diff_context_t*)diff_context;
-  for (int i = 0; i < NR_GPR; i++) {
-    state->XPR.write(i, (sword_t)ctx->gpr[i]);
+  void diff_init(int port)
+  {
+    p = get_core(0);
+    state = p->get_state();
   }
-  state->pc = ctx->pc;
-}
 
-void sim_t::diff_memcpy(reg_t dest, void* src, size_t n) {
-  mmu_t* mmu = p->get_mmu();
-  for (size_t i = 0; i < n; i++) {
-    mmu->store<uint8_t>(dest+i, *((uint8_t*)src+i));
+  void diff_step(uint64_t n)
+  {
+    // step(n);
+    p->step(n);
   }
-}
 
-extern "C" {
-
-__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  if (direction == DIFFTEST_TO_REF) {
-    s->diff_memcpy(addr, buf, n);
-  } else {
-    assert(0);
+  void diff_get_regs(void *diff_context)
+  {
+    struct diff_context_t *ctx = (struct diff_context_t *)diff_context;
+    for (int i = 0; i < NR_GPR; i++)
+    {
+      ctx->gpr[i] = state->XPR[i];
+    }
+    ctx->pc = state->pc;
   }
-}
 
-__EXPORT void difftest_regcpy(void* dut, bool direction) {
-  if (direction == DIFFTEST_TO_REF) {
-    s->diff_set_regs(dut);
-  } else {
-    s->diff_get_regs(dut);
+  void diff_set_regs(void *diff_context)
+  {
+    struct diff_context_t *ctx = (struct diff_context_t *)diff_context;
+    for (int i = 0; i < NR_GPR; i++)
+    {
+      state->XPR.write(i, (sword_t)ctx->gpr[i]);
+    }
+    state->pc = ctx->pc;
   }
-}
 
-__EXPORT void difftest_exec(uint64_t n) {
-  s->diff_step(n);
-}
+  void diff_memcpy(reg_t dest, void *src, size_t n)
+  {
+    mmu_t *mmu = p->get_mmu();
+    for (size_t i = 0; i < n; i++)
+    {
+      mmu->store<uint8_t>(dest + i, *((uint8_t *)src + i));
+    }
+  }
+};
 
-__EXPORT void difftest_init(int port) {
-  difftest_htif_args.push_back("");
-  const char *isa = "RV" MUXDEF(CONFIG_RV64, "64", "32") MUXDEF(CONFIG_RVE, "E", "I") "MAFDC";
-  cfg_t *cfg = new cfg_t(/*default_initrd_bounds=*/std::make_pair((reg_t)0, (reg_t)0),
-            /*default_bootargs=*/nullptr,
-            /*default_isa=*/isa,
-            /*default_priv=*/DEFAULT_PRIV,
-            /*default_varch=*/DEFAULT_VARCH,
-            /*default_misaligned=*/false,
-            /*default_endianness*/endianness_little,
-            /*default_pmpregions=*/16,
-            /*default_mem_layout=*/std::vector<mem_cfg_t>(),
-            /*default_hartids=*/std::vector<size_t>(1),
-            /*default_real_time_clint=*/false,
-            /*default_trigger_count=*/4);
-  s = new sim_t(cfg, false,
-      difftest_mem, difftest_plugin_devices, difftest_htif_args,
-      difftest_dm_config, nullptr, false, NULL,
-      false,
-      NULL,
-      true);
-  s->diff_init(port);
-}
+static cfg_t cfg;
+static diffsim_t *s = NULL;
 
-__EXPORT void difftest_raise_intr(uint64_t NO) {
-  trap_t t(NO);
-  p->take_trap_public(t, state->pc);
-}
+extern "C"
+{
 
+  __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
+  {
+    if (direction == DIFFTEST_TO_REF)
+    {
+      s->diff_memcpy(addr, buf, n);
+    }
+    else
+    {
+      assert(0);
+    }
+  }
+
+  __EXPORT void difftest_regcpy(void *dut, bool direction)
+  {
+    if (direction == DIFFTEST_TO_REF)
+    {
+      s->diff_set_regs(dut);
+    }
+    else
+    {
+      s->diff_get_regs(dut);
+    }
+  }
+
+  __EXPORT void difftest_exec(uint64_t n)
+  {
+    s->diff_step(n);
+  }
+
+  __EXPORT void difftest_init(int port)
+  {
+    difftest_htif_args.push_back("");
+    const char *isa = "RV" MUXDEF(CONFIG_RV64, "64", "32") MUXDEF(CONFIG_RVE, "E", "I") "MAFDC";
+    cfg.initrd_bounds = std::make_pair((reg_t)0, (reg_t)0);
+    cfg.bootargs = nullptr;
+    cfg.isa = isa;
+    cfg.priv = DEFAULT_PRIV;
+    cfg.misaligned = false;
+    cfg.endianness = endianness_little;
+    cfg.pmpregions = 16;
+    cfg.mem_layout = std::vector<mem_cfg_t>();
+    cfg.hartids = std::vector<size_t>(1);
+    cfg.real_time_clint = false;
+    cfg.trigger_count = 4;
+    cfg.external_simulator = std::nullopt;
+    if (s == NULL)
+    {
+      s = new diffsim_t(
+          &cfg, false,
+          difftest_mem, difftest_plugin_devices, difftest_htif_args,
+          difftest_dm_config, nullptr, false, NULL,
+          false,
+          NULL,
+          std::nullopt);
+    }
+    s->diff_init(port);
+  }
+
+  __EXPORT void difftest_raise_intr(uint64_t NO)
+  {
+    trap_t t(NO);
+    s->p->take_trap_public(t, s->state->pc);
+  }
 }
